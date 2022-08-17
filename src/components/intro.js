1771d2ba7e73dc9e73d9b32ee63e61b0fce5c30f
mhenden <mhenden@stud.hs-heilbronn.de>
2022-08-17 15:39:23 +0200

    adjust intro alignment

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,36 +1,35 @@
 export const Intro = (props) => {
     return (
-        <header className="intro">
-            <div className="intro__twosection">
-                <div>
-                    <h1 className="intro__hello">Hello!
-                        <span className="emoji wave-hand animated"></span>
-                    </h1>
-                    <h2 className="intro__tagline">I'm
-                        <span className="name"> Mertkan Henden</span>, a <span className="name">Data Scientist &amp; Web-Developer</span> focused on
-                        supporting the digital transformation of the mobility &amp; manufacturing sector
-                        <span className="emoji technologist"></span>
-                    </h2>
-                    <h3 className="intro__contact">
-                        <span>Get in touch </span>
-                        <span className="emoji pointer"></span>
-                        <span>
-                  <a href="mailto:mertkan.henden@gmx.de" target="_blank"
-                     className="highlight-link">mertkan.henden@gmx.de</a>
-                </span>
-                    </h3>
-                </div>
+        <div className="intro__twosection">
+            <header className="intro" style={{paddingBottom:"0px"}} >
 
+                    <div className="intro__tagline">
+                        <h1 className="intro__hello">Hello!
+                            <span className="emoji wave-hand animated"></span>
+                        </h1>
+                        <h2 className="intro__tagline">I'm
+                            <span className="name"> Mertkan Henden</span>, a <span className="name">Data Scientist &amp; Web-Developer</span> focused on
+                            supporting the digital transformation of the mobility &amp; manufacturing sector
+                            <span className="emoji technologist"></span>
+                        </h2>
+                        <h3 className="intro__contact">
+                            <span>Get in touch </span>
+                            <span className="emoji pointer"></span>
+                            <span>
+                      <a href="mailto:mertkan.henden@gmx.de" target="_blank"
+                         className="highlight-link">mertkan.henden@gmx.de</a>
+                    </span>
+                        </h3>
+                    </div>
+            </header>
 
+            <header className="picture_intro" >
                 <h2 className="intro__tagline">
                     <figure>
-                    <img src={props.image.img_url} alt="" />
+                        <img src={props.image.img_url} alt="" />
                     </figure>
                 </h2>
-
-            </div>
-
-        </header>
-
+            </header>
+        </div>
     )
 }
